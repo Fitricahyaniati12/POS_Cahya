4aa1f347b22cff727e6c5149b87bf3524d9989ea
Chy'aaaaa <mpittuks@gmail.com>
2025-03-08 11:28:18 +0700

    Praktikum 2.2 – Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,20 +8,11 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //public function profile($id, $name) {
-       // return view('user.profile')->with('id', $id)->with('name', $name);
-    
        public function index()
        {
-            // ambil model pertama yang cocok dengan batasan queri
-           //$user = UserModel::where('level_id',2)->first();
-
-            // alternatif untuk mengambil mode pertama yang cocok dengan batasan queri
-           //$user = UserModel::firstWhere('level_id', 3);
-
-           $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
-           return view('user', ['data' => $user]);
+        //$user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);
+        
        }
    }
