86332429374ae4aaa3c273b52a78ac9904d78614
Chy'aaaaa <mpittuks@gmail.com>
2025-03-08 12:52:57 +0700

    job 4 Practicum 2.5 – Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,29 +11,25 @@ class UserController extends Controller
     public function index()
     {
         $user = UserModel::create([
-            'username' => 'manager55',
-            'nama' => 'Manager55',
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2,
         ]);
 
-        $user->username = 'manager56';
+        // Mengubah username
+        $user->username = 'manager12';
 
-        $user->isDirty(); // true
-        $user->isDirty('username'); // true
-        $user->isDirty('nama'); // false
-        $user->isDirty(['nama', 'username']); // true
-
-        $user->isClean(); // false
-        $user->isClean('username'); // false
-        $user->isClean('nama'); // true
-        $user->isClean(['nama', 'username']); // false
-
+        // Menyimpan perubahan ke database
         $user->save();
 
-        $user->isDirty(); // false
-        $user->isClean(); // true
+        // Mengecek apakah ada perubahan setelah disimpan
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
 
-        dd($user->isDirty());
+        // Menampilkan hasil perubahan
+        dd($user->wasChanged(['nama', 'username'])); // true
     }
 }
