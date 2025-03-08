5f0bb426ddb901769ace3fe10580b1c9d2f26850
Chy'aaaaa <mpittuks@gmail.com>
2025-03-08 11:52:31 +0700

    Practicum 2.3 – Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,8 @@ class UserController extends Controller
 {
        public function index()
        {
-        //$user = UserModel::findOrFail(1);
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
+        ($user);
         return view('user', ['data' => $user]);
         
        }
