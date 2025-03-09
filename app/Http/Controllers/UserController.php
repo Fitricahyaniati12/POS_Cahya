4847196d537892830612ee77b7800331d09c77a2
Chy'aaaaa <mpittuks@gmail.com>
2025-03-09 13:06:09 +0700

    job 4 Practicum 2.6 – Create, Read, Update, Delete (CRUD)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,4 +30,31 @@ class UserController extends Controller
 
         return redirect()->route('user')->with('success', 'User berhasil ditambahkan');
     }
+
+     // Menampilkan form ubah pengguna
+     public function ubah($id)
+     {
+         $user = UserModel::find($id);
+         return view('user_ubah', ['data' => $user]);
+     }
+ 
+     // Menyimpan perubahan data pengguna
+     public function ubahSimpan(Request $request, $id)
+     {
+         $user = UserModel::find($id);
+         $user->update([
+             'username' => $request->username,
+             'nama' => $request->nama,
+             'level_id' => $request->level_id
+         ]);
+ 
+         return redirect()->route('user');
+     }
+ 
+     // Menghapus pengguna
+     public function hapus($id)
+     {
+         UserModel::find($id)->delete();
+         return redirect()->route('user');
+     }
 }
