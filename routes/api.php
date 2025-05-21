84db1b5d8a3dd973e6eae41c16727e38b6379df1
fitri cahya <mpittuks@gmail.com>
2025-05-21 20:21:58 +0700

    Job 9 Practicum 4 – Implementation of CRUD in RESTful API

--- routes/api.php
+++ routes/api.php
@@ -11,13 +11,13 @@ use App\Http\Controllers\Api\LevelController;
 Route::post('/register', RegisterController::class)->name('register');
 Route::post('/login', LoginController::class)->name('login');
 Route::post('/logout', LogoutController::class)->name('logout');
-/*Route::get('level', [LevelController::class, 'index']);
+Route::get('level', [LevelController::class, 'index']);
 Route::post('level', [LevelController::class, 'store']);
 Route::get('level/{level}', [LevelController::class, 'show']);
 Route::put('level/{level}', [LevelController::class, 'update']);
 Route::delete('level/{level}', [LevelController::class, 'destroy']);
 
-*/
+
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/user', function (Request $request) {
